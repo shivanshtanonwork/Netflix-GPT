68d22deea8d1696bf56eb8005d26d1a4237ca877
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-22 19:09:27 +0530

    NetflixGPT feature planning done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="font-bold bg-red-600">Hello Netflix</h1>
+      <h1 className="font-bold text-purple-900">Hello Netflix</h1>
     </>
   );
 }
